89080fdda117713b4c387903b4d20aa427a59ff3
avttd <aovedernikov@yandex.ru>
2019-09-10 13:34:20 +0300

    List

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -17,6 +17,26 @@ namespace Snake
             Point p2 = new Point(4,5,'#'); 
             p2.Draw();
 
+            List<int> numlist = new List<int>();
+            numlist.Add(10);
+            numlist.Add(20);
+            numlist.Add(30);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+
+            foreach(int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            numlist.RemoveAt(0);
+
+            List<Point> pList = new List<Point>;
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
 
         }
